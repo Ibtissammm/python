ba305fc606fe71d26a4f3b705055769cf434c26c
ibtissame <root@localhost.localdomain>
2021-03-13 06:49:10 -0500

    modif25

--- disable-jobs.py
+++ disable-jobs.py
@@ -3,7 +3,7 @@ import os
 server = jenkins.Jenkins('http://localhost:8080', username='admin', password='17001644')
 jobs = server.get_jobs()
 app_name=os.getenv("app_name")
-x=app_name
+x = app_name
 print(x)
 for job in jobs:
     if job['name'].startswith("x"):
