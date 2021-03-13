1b83ca6c81f904c7312bfa25990e37cd78eb08e2
ibtissame <root@localhost.localdomain>
2021-03-13 04:54:52 -0500

    modif4

--- disable-jobs.py
+++ disable-jobs.py
@@ -1,6 +1,6 @@
 import jenkins
 #import os
-server = jenkins.Jenkins('http://localhost:8080, username='admin', password='17001644')
+server = jenkins.Jenkins('http://localhost:8080', username='admin', password='17001644')
 jobs = server.get_jobs()
 #app_name=os.getenv('app_name')
 #for job in jobs :
