958b90c8efc20c8414cbcaf320349690c762da36
Ibtissammm <mohajir.ibtissame@gmail.com>
2021-04-04 01:01:42 +0200

    sonar script2

--- sonar.py
+++ sonar.py
@@ -2,5 +2,6 @@ from sonarqube import SonarQubeClient
 url = 'http://localhost:9000'
 username = "admin"
 password = "17001644"
-sonar = SonarQubeClient(sonarqube_url=url, username=username, password=password)components = list(sonar.components.search_components(qualifiers="TRK")
+sonar = SonarQubeClient(sonarqube_url=url, username=username, password=password)
+components = list(sonar.components.search_components(qualifiers="TRK")
 print('components')
