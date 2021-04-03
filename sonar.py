7469315513ed9799e4b880d5be9cb39f5ffb022d
Ibtissammm <mohajir.ibtissame@gmail.com>
2021-04-04 01:42:50 +0200

    sonar script8

--- sonar.py
+++ sonar.py
@@ -6,4 +6,4 @@ username = "admin"
 password = "admin"
 sonar = SonarQubeClient(sonarqube_url=url, username=username, password=password)
 components = list(sonar.components.search_components(qualifiers="TRK"))
-print('components')
+print(components)
